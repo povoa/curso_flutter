629a717ac316da2c8ef44dac0ee243635827d7e7
Joao Povoa <j.victorpovoa@gmail.com>
2020-07-11 18:49:37 -0300

    foi adicionada a função aos dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -17,22 +18,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rigthDiceNumber = 1;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rigthDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice1.png'),
-          )),
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      changeDiceFace();
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'))),
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice2.png'),
-          )),
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      changeDiceFace();
+                    });
+                  },
+                  child: Image.asset('images/dice$rigthDiceNumber.png'))),
         ],
       ),
     );
